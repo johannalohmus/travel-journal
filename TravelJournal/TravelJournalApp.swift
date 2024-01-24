bfe5fa1fce8feb00ecedbb6cf64993c663c47152
johannalohmus <johanna.lohmus@gmail.com>
2024-01-24 18:09:52 -0500

    Initial Commit

---
+++ TravelJournal/TravelJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelJournalApp.swift
+//  TravelJournal
+//
+//  Created by Johanna Lohmus on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
