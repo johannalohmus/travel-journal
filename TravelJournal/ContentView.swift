1b6b16d043ede3e011ad7599cc266d8fbd372348
johannalohmus <johanna.lohmus@gmail.com>
2024-01-24 18:18:55 -0500

    add text

--- TravelJournal/ContentView.swift
+++ TravelJournal/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack
+        {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hi, world!")
+            }
+            VStack {
+                Text("Yippee")
+            }
         }
+        
         .padding()
+        
     }
 }
 
