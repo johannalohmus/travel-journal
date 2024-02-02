766dab9b886f65965c9a5bc7b0a2e213765e41ee
johannalohmus <johanna.lohmus@gmail.com>
2024-02-01 22:41:36 -0500

    debug map

--- TravelJournal/Views/MapView.swift
+++ TravelJournal/Views/MapView.swift
@@ -18,9 +18,11 @@ struct MapView: View {
     }
     
     var body: some View {
-        let center = CLLocationCoordinate2D(latitude: 39.5, longitude: -98.0)
-        Map(initialViewport: .camera(center: center, zoom: 2, bearing: 0, pitch: 0))
+        let middle = CLLocationCoordinate2D(latitude: 39.5, longitude: -98.0)
+        Map(initialViewport: .camera(center: middle, zoom: 2, bearing: 0, pitch: 0))
         .ignoresSafeArea()
+        
+        
     }
 }
 
