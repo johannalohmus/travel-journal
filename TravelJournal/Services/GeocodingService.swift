e9079b42fb03ffc0493d13380b6533c5b5043770
johannalohmus <johanna.lohmus@gmail.com>
2024-02-08 13:31:35 -0500

    add geocodingservice

---
+++ TravelJournal/Services/GeocodingService.swift
@@ -0,0 +1,20 @@
+import MapboxGeocoding
+import Combine
+
+class GeocodingService: ObservableObject {
+    private let geocodingClient = Geocoding(accessToken: "YOUR_MAPBOX_ACCESS_TOKEN")
+    private var cancellables: Set<AnyCancellable> = []
+
+    @Published var searchResults: [GeocodedPlacemark] = []
+
+    func search(query: String) {
+        geocodingClient.geocode(ForwardGeocodeOptions(query: query)) { [weak self] (result) in
+            switch result {
+            case .success(let response):
+                self?.searchResults = response.features
+            case .failure(let error):
+                print("Geocoding error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
